2fa139fc804886ebde9f607e1924950070bd5b2e
sebastianps45 <jpolancos@ucentral.edu.co>
2026-09-02 20:48:38 -0500

    7
    vamo a ver

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ app = FastAPI(title="Generador de cédula")
 def obtener_cedula() -> dict:
     numero = random.randint(1000000000, 9999999999)
     return {"cedula": numero}
+
+
+@app.get("/doblar")
+def doblar_numero(n: int) -> dict:
+    """Recibe un número entero `n` y devuelve el número multiplicado por 2."""
+    return {"resultado": n * 2}
